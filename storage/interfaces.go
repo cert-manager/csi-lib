52d5196a673310ec228d06c6120c81fab7570d4a
joshvanl <vleeuwenjoshua@gmail.com>
2021-06-25 11:15:16 +0100

    Update DataWriter WriteFiles func to accept a metadata, rather than volume ID
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- storage/interfaces.go
+++ storage/interfaces.go
@@ -71,8 +71,5 @@ type MetadataWriter interface {
 // DataWriter is used to write data (e.g. certificate and private keys) to the
 // storage backend.
 type DataWriter interface {
-	// WriteFiles will write the given data to the specified file names in the
-	// volumes data directory. Sets the file user ownership to fsUser UID. If
-	// fsUser is nil, ownership will not change.
-	WriteFiles(volumeID string, files map[string][]byte, fsUser *int64) error
+	WriteFiles(meta metadata.Metadata, files map[string][]byte) error
 }
